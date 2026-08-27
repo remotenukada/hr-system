649ddfaccc279087f3293d24dad3d250488068fc
server-dell <remote.nukada@gmail.com>
2026-08-27 16:02:23 +0900

    feat: add employee scheduled work conditions

---
+++ prisma/migrations/20260827065357_add_employee_scheduled_work_conditions/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Employee" ADD COLUMN     "annualScheduledDays" INTEGER,
+ADD COLUMN     "dailyScheduledHours" DOUBLE PRECISION,
+ADD COLUMN     "weeklyScheduledDays" DOUBLE PRECISION,
+ADD COLUMN     "weeklyScheduledHours" DOUBLE PRECISION;
